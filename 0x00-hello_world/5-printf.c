d337facf5279d9d2615b164ddadf36c4b96a6d1e
Chigoziee <61709944+Chigoziee@users.noreply.github.com>
2022-10-13 19:45:06 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,29 +1,15 @@
 #include<stdio.h>
-
 /**
- *
- * *main - prints the phrase with proper grammar,
- *
- * * but the outcome is a piece of art,
- *
- * *Return: 0 if run succesfuly, non zero otherwise
- *
- * */
-
-
+*main - prints the phrase with proper grammar,
+* but the outcome is a piece of art,
+*Return: 0 if run succesfuly, non zero otherwise
+*/
 
 int main(void)
-
 {
 
+printf("with proper grammar, but the outcome is a piece of art,\n");
 
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-
-
-
-
-
-	return (0);
-
+return (0);
 }
