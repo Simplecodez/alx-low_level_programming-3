6be5baefb13e20858227233b23decf072128b53b
Chigoziee <61709944+Chigoziee@users.noreply.github.com>
2022-10-14 19:59:47 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,35 +1,20 @@
 #include <stdio.h>
 
-
-
 /**
- *
- *  * main - prints the alphabet in lowercase, and then in uppercase,
- *
- *   * followed by a new line
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * main - prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	int chr;
 
-		int chr;
-
-
-
-			for (chr = 'a'; chr <= 'z'; chr++)
-
-						putchar(chr);
-
-				for (chr = 'A'; chr <= 'Z'; chr++)
-
-							putchar(chr);
-
-					putchar('\n');
-
-						return (0);
-
+	for (chr = 'a'; chr <= 'z'; chr++)
+		putchar(chr);
+	for (chr = 'A'; chr <= 'Z'; chr++)
+		putchar(chr);
+	putchar('\n');
+	return (0);
 }
+Footer
+© 2022 GitHub, Inc
