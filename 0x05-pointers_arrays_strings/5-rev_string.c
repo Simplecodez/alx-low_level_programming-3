e12fac7e94eb905aefa9c661c76746083d66b779
Chigoziee <sk.hyginus@gmail.com>
2022-10-24 13:52:28 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -7,14 +7,15 @@
  */
 void rev_string(char *s)
 {
- int i, len, temp;  
-    len = strlen(s);  
-    for (i = 0; i < len/2; i++)  
-    {   
-        temp = s[i];  
-        s[i] = s[len - i - 1];  
-        s[len - i - 1] = temp;  
-    }  
+int i, len, temp;  
+len = strlen(s);  
+for (i = 0; i < len/2; i++)  
+{   
+temp = s[i];  
+s[i] = s[len - i - 1];  
+s[len - i - 1] = temp;  
+}
+
 }
 
 
