2e9a2bb2d299b4be60af6df37f42ec3220e563ad
Chigoziee <sk.hyginus@gmail.com>
2022-10-24 12:21:17 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - calculates the lenght of a string
+ * @s: The pointer to an int.
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+
+
+}
