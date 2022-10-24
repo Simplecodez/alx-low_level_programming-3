3443966dbd12fd5fbc27b7b056d04cf4bcfabca7
Chigoziee <sk.hyginus@gmail.com>
2022-10-24 11:18:17 -0700

    executed task c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * function that takes a pointer to an int as parameter and * updates the value it points to.
+ * print_square - Prints a squareusing the character #.
+ * @size: The size of the square.
  */
-
 void reset_to_98(int *n)
 {
 *n = 98;
