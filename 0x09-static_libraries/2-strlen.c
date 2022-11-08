6f04a4f13382016e0be6e47ba52766198a728499
Chigoziee <sk.hyginus@gmail.com>
2022-11-08 02:00:25 -0800

    static library code

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - calculates the lenght of a string
+ * @s: The pointer to an int
+ * Return: lenght of string.
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+
+
+}
